fc4aca91b9de7bce395350e6d8f61fcf861637d9
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-16 22:46:00 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_attached_file :avatar, default_url: ':style/default.jpg', style: { thumb: "100x100>" }
+  has_attached_file :avatar, default_url: 'default.jpg', style: { thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def artist_name
